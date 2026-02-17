efb6d4a5d9af4ab9977f0d58ed46c5d6e012077f
Krish Bhuva <bhuvakrish03@gmail.com>
2026-02-17 21:00:33 +0530

    Refactor work experience cards with structured data and updated UI, refresh tech stack components, and manage assets.

--- src/components/About/Intrestedstack.js
+++ src/components/About/Intrestedstack.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import {
-  SiKalilinux
-} from "react-icons/si";
+import { SiKalilinux } from "react-icons/si";
 import { MdSecurity } from "react-icons/md";
 import { GiBrain } from "react-icons/gi";
+
+const interestedItems = [
+  { icon: <GiBrain />, name: "AI / ML" },
+  { icon: <MdSecurity />, name: "Cybersecurity" },
+  { icon: <SiKalilinux />, name: "Kali Linux" },
+];
 
 function Intrestedstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <GiBrain />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <MdSecurity />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiKalilinux />
-      </Col>
+      {interestedItems.map((item, index) => (
+        <Col xs={4} md={2} className="tech-icons" key={index}>
+          {item.icon}
+          <p className="tech-icon-label">{item.name}</p>
+        </Col>
+      ))}
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -11,55 +11,37 @@ import {
   SiMacos,
   SiIntellijidea,
   SiInsomnia,
-  SiSelenium
+  SiSelenium,
 } from "react-icons/si";
 
 import { BiLogoVisualStudio } from "react-icons/bi";
-
 import { FaAws } from "react-icons/fa6";
+
+const toolItems = [
+  { icon: <SiMacos />, name: "macOS" },
+  { icon: <SiLinux />, name: "Linux" },
+  { icon: <FaAws />, name: "AWS" },
+  { icon: <BiLogoVisualStudio />, name: "VS Code" },
+  { icon: <SiIntellijidea />, name: "IntelliJ" },
+  { icon: <SiEclipseche />, name: "Eclipse" },
+  { icon: <SiApachenetbeanside />, name: "NetBeans" },
+  { icon: <SiSelenium />, name: "Selenium" },
+  { icon: <SiPostman />, name: "Postman" },
+  { icon: <SiInsomnia />, name: "Insomnia" },
+  { icon: <SiDocker />, name: "Docker" },
+  { icon: <SiVercel />, name: "Vercel" },
+  { icon: <SiVmware />, name: "VMware" },
+];
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiLinux />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaAws />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <BiLogoVisualStudio />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiIntellijidea />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiEclipseche />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiApachenetbeanside />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSelenium />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiInsomnia />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDocker />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVmware />
-      </Col>
+      {toolItems.map((item, index) => (
+        <Col xs={4} md={2} className="tech-icons" key={index}>
+          {item.icon}
+          <p className="tech-icon-label">{item.name}</p>
+        </Col>
+      ))}
     </Row>
   );
 }
