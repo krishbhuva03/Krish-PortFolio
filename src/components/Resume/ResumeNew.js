b23f76776cee175cc1b1d0e4a02c0cfb5f72a70e
Krish Bhuva <bhuvakrish03@gmail.com>
2026-06-23 19:05:24 +0530

    feat: update resume document to Krish_Resume.pdf

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/New_Resume_2026.pdf";
+import pdf from "../../Assets/Krish_Resume..pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
